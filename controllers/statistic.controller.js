cb8671e51c0cfbc6b731cf555b39dfbd2d8cacc5
Lilyhouser <houselily010@gmail.com>
2026-02-27 11:27:01 +0700

    add statistic

---
+++ controllers/statistic.controller.js
@@ -0,0 +1,11 @@
+const getStatisticByGenderAgeMode = async (req, res) => {
+
+};
+
+const getStatisticByUserNumber = async (req, res) => {
+    // Thống kê số lượng user hoạt động hôm nay
+}
+
+module.exports = {
+    getStatisticByGenderAgeMode
+}
